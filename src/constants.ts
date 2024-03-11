cbc759edf58b04cf56e82dc14c5b52b1e22a00d7
itchyny <itchyny@cybozu.co.jp>
2024-03-11 18:17:10 +0900

    implement action for single file

--- src/constants.ts
+++ src/constants.ts
@@ -6,8 +6,13 @@ export enum Inputs {
   AWSRegion = "aws-region",
   AWSAccessKeyId = "aws-access-key-id",
   AWSSecretAccessKey = "aws-secret-access-key",
+  AWSSessionToken = "aws-session-token",
 }
 
 export enum Outputs {
   CacheHit = "cache-hit",
+}
+
+export enum State {
+  CacheMatchedKey = "CACHE_MATCHED_KEY",
 }
--- src/restore.ts
+++ src/restore.ts
@@ -1,28 +1,44 @@
+import * as fs from "fs";
 import * as core from "@actions/core";
-import { Inputs } from "./constants";
+import * as s3 from "@aws-sdk/client-s3";
+import { Inputs, Outputs, State } from "./constants";
+import { S3Client } from "./s3-client";
 
 async function restore() {
   try {
     const path = core.getInput(Inputs.Path, { required: true });
     const key = core.getInput(Inputs.Key, { required: true });
     const restoreKeys = core.getInput(Inputs.RestoreKeys);
-    const bucketName = core.getInput(Inputs.BucketName, { required: true });
-    const awsRegion = core.getInput(Inputs.AWSRegion, { required: true });
-    const awsAccessKeyId = core.getInput(Inputs.AWSAccessKeyId, {
-      required: true,
-    });
-    const awsSecretAccessKey = core.getInput(Inputs.AWSSecretAccessKey, {
-      required: true,
-    });
     core.debug(`${Inputs.Path}: ${path}`);
     core.debug(`${Inputs.Key}: ${key}`);
     core.debug(`${Inputs.RestoreKeys}: ${restoreKeys}`);
-    core.debug(`${Inputs.BucketName}: ${bucketName}`);
-    core.debug(`${Inputs.AWSRegion}: ${awsRegion}`);
-    core.debug(`${Inputs.AWSAccessKeyId}: ${awsAccessKeyId.replace(/.*/g, "***")}`);
-    core.debug(`${Inputs.AWSSecretAccessKey}: ${awsSecretAccessKey.replace(/.*/g, "***")}`);
+
+    const client = new S3Client();
+    const stream = fs.createWriteStream(`${path}.tmp`);
+    try {
+      await client.getObject(key, stream);
+      fs.renameSync(`${path}.tmp`, path);
+      core.saveState(State.CacheMatchedKey, key);
+      core.setOutput(Outputs.CacheHit, "true");
+      core.info(`Cache restored from S3 with key: ${key}`);
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        if (error instanceof s3.NoSuchKey) {
+          core.info(`Cache not found in S3 with key: ${key}`);
+          return;
+        }
+        core.warning(error.message);
+      }
+    } finally {
+      stream.destroy();
+      if (fs.existsSync(`${path}.tmp`)) {
+        fs.unlinkSync(`${path}.tmp`);
+      }
+    }
   } catch (error: unknown) {
-    core.setFailed((error as Error).message);
+    if (error instanceof Error) {
+      core.setFailed(error.message);
+    }
   }
 }
 
